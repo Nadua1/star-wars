d5df9710921cbb6ffd0768e08fbb498e44227a7f
Nadua <vladickschaf@gmail.com>
2025-07-06 14:17:23 +0300

    third commint

--- src/components/DreamTeam.jsx
+++ src/components/DreamTeam.jsx
@@ -1,30 +1,13 @@
 import React from 'react';
-import friend1 from "../assets/friend1.jpg";
-import friend2 from "../assets/friend2.jpg";
-import friend3 from "../assets/friend3.jpg";
-import friend4 from "../assets/friend4.jpg";
-import friend5 from "../assets/friend5.jpg";
-import friend9 from "../assets/friend9.jpg";
-import friend7 from "../assets/friend7.jpg";
-import friend8 from "../assets/friend8.jpg";
-import friend6 from "../assets/friend6.jpg";
-import Foto from "./Foto.jsx";
+
 import {itemsFoto} from "../utils/constants.js";
 
 const DreamTeam = () => {
     return (
         <section className="float-end row w-50 border rounded-bottom-4 me-0 ms-2">
             <h2 className="text-center">Dream Team</h2>
-            {/*{itemsFoto.map((item=> <Foto itemFoto={item} key={item}/>))}*/}
-            <Foto itemFoto={friend1}/>
-            <Foto itemFoto={friend2}/>
-            <Foto itemFoto={friend3}/>
-            <Foto itemFoto={friend4}/>
-            <Foto itemFoto={friend5}/>
-            <Foto itemFoto={friend9}/>
-            <Foto itemFoto={friend7}/>
-            <Foto itemFoto={friend8}/>
-            <Foto itemFoto={friend6}/>
+            {itemsFoto.map((item=> <Foto itemFoto={item} key={item}/>))}
+
         </section>
     );
 };
