8bb1198f426d498bc1895761bf3dad29b45695ff
Nadua <vladickschaf@gmail.com>
2025-07-09 12:20:54 +0300

    about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {base_url} from "../utils/constants.js";
 
 const AboutMe = () => {
-    return (
-        <div>
-            About Me
-        </div>
-    );
+    const [aboutMe, setAboutMe] = useState('');
+    useEffect(() => {
+        const name = "Luke Skywalker";
+        fetch(`${base_url}v1/peoples/${name}`)
+            .then(res => res.json())
+            .then(data => setAboutMe(data.aboutMe));
+        return () => {console.log("done")}
+    }, [])
+    if(aboutMe){
+        return (<p>{aboutMe}</p>)
+    }else {
+        return (<p>"error"</p>)
+    }
+   
 };
 
 export default AboutMe;
